d1b224ff2556167519233ba5ea230a9a4ea2876e
valorpulse <lukas.jakubauskas.dev@gmail.com>
2026-02-20 08:58:35 -0500

    code within one async block executes linearly

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,30 @@
 use trpl;
+use std::time::Duration;
 
 fn main() {
     trpl::block_on(async {
         let (tx, mut rx) = trpl::channel();
 
-        let vals = vec! [
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("other"),
-            String::from("side"),
-        ];
+        let tx_fut = async {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("future"),
+            ];
 
-        for val in vals {
-            tx.send(val).unwrap();
-            trpl::sleep(std::time::Duration::from_millis(500)).await;
-        }
+            for val in vals {
+                tx.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
 
-        while let Some(val) = rx.recv().await {
-            println!("got: {}", val);
-        }
+        let rx_fut = async {
+            while let Some(value) = rx.recv().await {
+                println!("received '{value}'");
+            }
+        };
+
+        trpl::join(tx_fut, rx_fut).await;
     })    
 }
